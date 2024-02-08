3049849df7535760386dba47d4e5e70ad87c5739
root <root@skr-hydraulic.com.ua>
2024-02-08 18:20:09 +0200

    fix

--- resources/views/frontend/templates/blog/include/items.blade.php
+++ resources/views/frontend/templates/blog/include/items.blade.php
@@ -1,13 +1,23 @@
 @foreach($articles as $article)
-    <div class="blog__item">
-        <a href="{{ $article->alias }}">
-            <div class="blog__item-img">
-                <picture>
-                    <source type="image/webp" srcset="{{ $article->getImageUrl('page_blog', 'image', true) }}">
-                    <img src="{{ $article->getImageUrl('page_blog') }}"
-                         alt="{{ $article->name }}"
-                         title="{{ $article->name }}">
+<div class="blog__item">
+    <a href="{{ $article->alias }}">
+        <div class="blog__item-img">
+            <?php  $page_add_fields = $article->page_template->page_additional_field; ?>
+            @foreach($page_add_fields as $field)
+            @php($value = $field->page_additional_field_value->where('page_id', $article->id)->first() && $field->page_additional_field_value->where('page_id', $article->id)->first()->translate()
+            ? $field->page_additional_field_value->where('page_id', $article->id)->first()->translate()->value
+            : null)
+            @php($type = $field->page_additional_field_type->type)
+            @if($type == 'file' && $value != '' && Storage::disk('public')->exists(\App\Models\Page\Page::GALLERY_PATH. $article->id . '/' . $value))
+            <picture>
+                <img src="{{ asset("/storage/pages/$article->id/$value") }}"  alt="{{ $article->name }}" title="{{ $article->name }}">
+
+                <source type="image/webp" srcset="{{ asset("/storage/pages/$article->id/$value") }}">
                 </picture>
+                @endif
+                @endforeach
+                
+                
             </div>
             <div class="blog__item-title">
                 {{ $article->name }}
@@ -18,4 +28,4 @@
             </div>
         </a>
     </div>
-@endforeach
+    @endforeach
--- resources/views/frontend/templates/blog_one/index.blade.php
+++ resources/views/frontend/templates/blog_one/index.blade.php
@@ -8,109 +8,112 @@
 @section('canonical', $seo_meta['seo_canonical'])
 @include('frontend.elements.ogMeta')
 @section('blogSeo')
-    @include('frontend.templates.blog_one.include.blogSeo')
+@include('frontend.templates.blog_one.include.blogSeo')
 @endsection
 
 @section('styles')
-    @parent
+@parent
 
-    {!! Html::style(mix('/assets/frontend/css/blog-one.min.css')) !!}
+{!! Html::style(mix('/assets/frontend/css/blog-one.min.css')) !!}
 @endsection
 
 @section('scripts')
-    @parent
+@parent
 
-    {!! Html::script(mix('/assets/frontend/js/main.js')) !!}
+{!! Html::script(mix('/assets/frontend/js/main.js')) !!}
 @endsection
 
 @section('content')
-    <main>
-        <div class="blog blog-one__page">
-            <div class="page--line">
-                <div class="container">
-                    <div class="row">
-                        @include('frontend.elements.breadcrumbs')
-                    </div>
+<main>
+    <div class="blog blog-one__page">
+        <div class="page--line">
+            <div class="container">
+                <div class="row">
+                    @include('frontend.elements.breadcrumbs')
                 </div>
             </div>
-            <div class="container">
-                <div class="blog-one__inner">
-                    <div class="blog-one__content article">
-                        <div class="article__title">
-                            {{ $page->name }}
-                        </div>
-                        @include('frontend.templates.blog_one.include.info')
-                        <picture>
-                            <?php  $page_add_fields = $page->page_template->page_additional_field; ?>
-                             @foreach($page_add_fields as $field)
-                            @php($value = $field->page_additional_field_value->where('page_id', $page->id)->first() && $field->page_additional_field_value->where('page_id', $page->id)->first()->translate()
-                                  ? $field->page_additional_field_value->where('page_id', $page->id)->first()->translate()->value
-                                  : null)
-                            @php($type = $field->page_additional_field_type->type)
-                                    @if($type == 'file' && $value != '' && Storage::disk('public')->exists(\App\Models\Page\Page::GALLERY_PATH. $page->id . '/' . $value))
-                                        <img src="{{ asset("/storage/pages/$page->id/$value") }}"  alt="{{ $page->name }}" title="{{ $page->name }}">
+        </div>
+        <div class="container">
+            <div class="blog-one__inner">
+                <div class="blog-one__content article">
+                    <div class="article__title">
+                        {{ $page->name }}
+                    </div>
+                    @include('frontend.templates.blog_one.include.info')
+                    
+                    <?php  $page_add_fields = $page->page_template->page_additional_field; ?>
+                    @foreach($page_add_fields as $field)
+                    @php($value = $field->page_additional_field_value->where('page_id', $page->id)->first() && $field->page_additional_field_value->where('page_id', $page->id)->first()->translate()
+                    ? $field->page_additional_field_value->where('page_id', $page->id)->first()->translate()->value
+                    : null)
+                    @php($type = $field->page_additional_field_type->type)
+                    @if($type == 'file' && $value != '' && Storage::disk('public')->exists(\App\Models\Page\Page::GALLERY_PATH. $page->id . '/' . $value))
+                    <picture>
+                        <img src="{{ asset("/storage/pages/$page->id/$value") }}"  alt="{{ $page->name }}" title="{{ $page->name }}">
 
-                                        <source type="image/webp" srcset="{{ asset("/storage/pages/$page->id/$value") }}">
-                           @endif
-                                    @endforeach
-                            
+                        <source type="image/webp" srcset="{{ asset("/storage/pages/$page->id/$value") }}">
                         </picture>
+                        
+                        @endif
+                        @endforeach
+                        
+                        
                         {!!  $page->description !!}
                         @include('frontend.templates.blog_one.include.info')
                     </div>
                     <div class="blog-one__aside">
                         @if($page->products->isNotEmpty())
-                            <div class="blog-one__aside-checkout">
-                                <div class="checkout__aside-title">
-                                    @lang('frontend/content/index.used_product_in_article'):
-                                </div>
-                                @foreach($page->products as $product)
-                                    <a href="{{ $product->alias }}" target="_blank">
-                                        <div class="checkout__aside-item">
-                                            <picture>
-                                                <source type="image/webp" srcset="{{ $product->cover->getUrl('prod_md', true) }}">
-                                                <img class="checkout__aside-img"
-                                                     src="{{ $product->cover->getUrl('prod_md') }}"
-                                                     alt="{{ $product->cover->getImageAlt() }}"
-                                                     title="{{ $product->cover->getImageAlt() }}">
-                                            </picture>
-                                            <div class="checkout__aside-info">
-                                                <div class="checkout__aside-text">{{ $product->name }}</div>
-                                                <div class="checkout__aside-number">
-                                                    <div class="checkout__aside-prise">
-                                                        {{ $product->format_price }} <span>@lang('frontend/product/index.uah')</span>
-                                                    </div>
+                        <div class="blog-one__aside-checkout">
+                            <div class="checkout__aside-title">
+                                @lang('frontend/content/index.used_product_in_article'):
+                            </div>
+                            @foreach($page->products as $product)
+                            <a href="{{ $product->alias }}" target="_blank">
+                                <div class="checkout__aside-item">
+                                    <picture>
+                                        <source type="image/webp" srcset="{{ $product->cover->getUrl('prod_md', true) }}">
+                                            <img class="checkout__aside-img"
+                                            src="{{ $product->cover->getUrl('prod_md') }}"
+                                            alt="{{ $product->cover->getImageAlt() }}"
+                                            title="{{ $product->cover->getImageAlt() }}">
+                                        </picture>
+                                        <div class="checkout__aside-info">
+                                            <div class="checkout__aside-text">{{ $product->name }}</div>
+                                            <div class="checkout__aside-number">
+                                                <div class="checkout__aside-prise">
+                                                    {{ $product->format_price }} <span>@lang('frontend/product/index.uah')</span>
                                                 </div>
                                             </div>
                                         </div>
-                                    </a>
+                                    </div>
+                                </a>
                                 @endforeach
                             </div>
-                        @endif
-                        <div class="latest-publications">
-                            <div class="latest-publications__title">
-                                @lang('frontend/content/index.last_published'):
-                            </div>
-                            @foreach($articles as $article)
+                            @endif
+                            <div class="latest-publications">
+                                <div class="latest-publications__title">
+                                    @lang('frontend/content/index.last_published'):
+                                </div>
+                                @foreach($articles as $article)
                                 @if( $article->alias != $page->alias )
-                                    <a href="{{ $article->alias }}">
-                                        <div class="latest-publications__item">
-                                            <div class="latest-publications__date">
-                                                <i class="icon icon-calendar"></i>
-                                                {{ $article->updated_at->translatedFormat('j F Y') }}
-                                            </div>
-                                            <div class="latest-publications__text">
-                                                {{ $article->name }}
-                                            </div>
+                                <a href="{{ $article->alias }}">
+                                    <div class="latest-publications__item">
+                                        <div class="latest-publications__date">
+                                            <i class="icon icon-calendar"></i>
+                                            {{ $article->updated_at->translatedFormat('j F Y') }}
                                         </div>
-                                    </a>
+                                        <div class="latest-publications__text">
+                                            {{ $article->name }}
+                                        </div>
+                                    </div>
+                                </a>
                                 @endif
-                            @endforeach
+                                @endforeach
+                            </div>
+                            @include('frontend.elements.forms.subscribe-blog')
                         </div>
-                        @include('frontend.elements.forms.subscribe-blog')
                     </div>
                 </div>
             </div>
-        </div>
-    </main>
-@endsection
+        </main>
+        @endsection
