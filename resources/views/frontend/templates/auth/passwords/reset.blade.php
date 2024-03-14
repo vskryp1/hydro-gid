dd73da16a1e952b0bb1e5bb6efb49abb76bc381f
roman-a-k <romankaretnichenko@gmail.com>
2024-03-14 15:14:46 +0200

    Sk-15

--- resources/views/frontend/templates/auth/passwords/reset.blade.php
+++ resources/views/frontend/templates/auth/passwords/reset.blade.php
@@ -15,32 +15,34 @@
 @endsection
 
 @section('content')
-    <div class="container" id="modal-reset">
-        <div class="modal__f">
-            <div class="modal__inner modal__form">
-                <div class="ttl-top modal__title">
+    <main>
+        <div class="container" id="modal-reset">
+            <div class="modal__f">
+                <div class="modal__inner modal__form">
+                    <div class="ttl-top modal__title">
                         <span class="item-ttl">
                             @lang('frontend.forgot_password')
                         </span>
-                </div>
-                <div class="modal__content">
-                    {{ Form::open(['id' => 'reset-password', 'url' => route("password.update")]) }}
-                    {{ Form::hidden('token', $token) }}
-                    <div class="form-field input-field" data-error="@lang('frontend.required_field')">
-                        {{ Form::email('email', $email, ['class' => 'validate form-control', 'required', 'data-validate' => 'email', 'readonly', 'placeholder' => __('frontend.your_email')]) }}
                     </div>
-                    <div class="form-field input-field" data-error="@lang('frontend.required_field')">
-                        {{ Form::password('password', ["class" => "validate form-control", "required", 'placeholder' => __('frontend.password')]) }}
-                    </div>
-                    <div class="form-field input-field" data-error="@lang('frontend.required_field')">
-                        {{ Form::password('password_confirmation', ['class' => 'validate form-control', 'required', 'placeholder' => __('frontend.repeat_password')]) }}
-                    </div>
+                    <div class="modal__content">
+                        {{ Form::open(['id' => 'reset-password', 'url' => route("password.update")]) }}
+                        {{ Form::hidden('token', $token) }}
+                        <div class="form-field input-field" data-error="@lang('frontend.required_field')">
+                            {{ Form::email('email', $email, ['class' => 'validate form-control', 'required', 'data-validate' => 'email', 'readonly', 'placeholder' => __('frontend.your_email')]) }}
+                        </div>
+                        <div class="form-field input-field" data-error="@lang('frontend.required_field')">
+                            {{ Form::password('password', ["class" => "validate form-control", "required", 'placeholder' => __('frontend.password')]) }}
+                        </div>
+                        <div class="form-field input-field" data-error="@lang('frontend.required_field')">
+                            {{ Form::password('password_confirmation', ['class' => 'validate form-control', 'required', 'placeholder' => __('frontend.repeat_password')]) }}
+                        </div>
 
-                    <button type="submit"
-                            class="submit">@lang('frontend.save_data')</button>
-                    {{ Form::close() }}
+                        <button type="submit"
+                                class="submit">@lang('frontend.save_data')</button>
+                        {{ Form::close() }}
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
+    </main>
 @endsection
