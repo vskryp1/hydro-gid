5b3c75239e2134935657a204e8f96b25d6e997dc
roman-a-k <romankaretnichenko@gmail.com>
2024-06-18 21:57:39 +0300

    SK-22 paginate

--- resources/views/vendor/pagination/bootstrap-4.blade.php
+++ resources/views/vendor/pagination/bootstrap-4.blade.php
@@ -10,9 +10,9 @@
         {{-- Pagination Elements --}}
         @foreach ($elements as $element)
             {{-- "Three Dots" Separator --}}
-            @if (is_string($element))
-                <li class="page-item disabled" aria-disabled="true"><span class="page-link">{{ $element }}</span></li>
-            @endif
+{{--            @if (is_string($element))--}}
+{{--                <li class="page-item disabled" aria-disabled="true"><span class="page-link">{{ $element }}</span></li>--}}
+{{--            @endif--}}
 
             {{-- Array Of Links --}}
             @if (is_array($element))
